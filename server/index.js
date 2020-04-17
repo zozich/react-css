4cda3996783d5369016188baeef8fdc22c436bae
akoziulia <akoziulia@cogniance.com>
2020-04-17 04:05:35 +0300

    Added simple server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+const emails = [{
+  id: 1,
+  topic: 'Email topic'
+}, {
+  id: 2,
+  topic: 'Email topic 2'
+}];
+
+app.get('/emails', function (req, res) {
+  const {page = 1, pageSize = 5} = req.query;
+  console.log(page);
+  console.log(pageSize);
+
+  res.send(emails);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
